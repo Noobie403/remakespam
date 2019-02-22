b1d82ff8404d2cc876c8c1c8fb32344d37621e8b
Noobie403 <47475293+Noobie403@users.noreply.github.com>
2019-02-22 21:45:05 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("543752674055553029")
+setInterval(function() {
+channel.send(`RemAKE CREDIT SERVER ,, 123 REMMMMMAKE ISISISIS HEREREERERERE MOTHER LOVERS :) BEAS.`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
